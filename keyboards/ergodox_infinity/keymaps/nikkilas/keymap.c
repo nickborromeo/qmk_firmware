38c1403694217049805e40ff0a358db7c39bfa4f
Nick Borromeo <ngborromeo@gmail.com>
2018-10-18 23:14:15 -0700

    setup ergodox keymap

--- keyboards/ergodox_infinity/keymaps/nikkilas/keymap.c
+++ keyboards/ergodox_infinity/keymaps/nikkilas/keymap.c
@@ -1,5 +1,5 @@
 #include QMK_KEYBOARD_H
-/* #include "ergodox_ez.h" */
+#include "ergodox_infinity.h"
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
@@ -21,11 +21,11 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = KEYMAP(KC_GRAVE,KC_1,KC_2,KC_3,KC_4,KC_5,LALT(LGUI(KC_F)),KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_PIPE,KC_ESCAPE,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSPO,CTL_T(KC_Z),KC_X,KC_C,KC_V,KC_B,KC_EQUAL,MO(2),LALT(LGUI(KC_H)),LALT(LGUI(KC_L)),KC_LALT,KC_LGUI,KC_LABK,KC_RABK,KC_PERC,KC_SPACE,CTL_T(KC_ESCAPE),LCTL(KC_Z),LCTL(KC_SPACE),KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_UNDS,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,KC_COLN,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSPC,KC_LBRACKET,KC_RBRACKET,LALT(LCTL(LGUI(KC_LEFT))),LALT(LCTL(LGUI(KC_RIGHT))),MO(1),KC_LCBR,KC_RCBR,KC_AT,KC_COMMA,KC_ENTER,KC_BSPACE),
+  [0] = KEYMAP(KC_GRAVE,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,LALT(LGUI(KC_F)),KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_PIPE,KC_ESCAPE,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSPO,CTL_T(KC_Z),KC_X,KC_C,KC_V,KC_B,KC_EQUAL,MO(2),LALT(LGUI(KC_H)),LALT(LGUI(KC_L)),KC_LALT,KC_LGUI,KC_LCBR,KC_LBRACKET,LCTL(LGUI(KC_SPACE)),KC_SPACE,CTL_T(KC_ESCAPE),MO(1),LCTL(KC_SPACE),KC_CIRC,KC_AMPR,KC_ASTR,KC_TILD,KC_PLUS,KC_MINUS,KC_UNDS,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_DQUO,KC_COLN,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSPC,KC_LABK,KC_RABK,LALT(LCTL(LGUI(KC_LEFT))),LALT(LCTL(LGUI(KC_RIGHT))),MO(1),KC_RBRACKET,KC_RCBR,KC_QUOTE,KC_COMMA,KC_ENTER,KC_BSPACE),
 
-  [1] = KEYMAP(KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_TRANSPARENT,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,KC_TRANSPARENT,KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LPRN,KC_RPRN,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_LBRACKET,KC_RBRACKET,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGB_MOD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_VAD,RGB_VAI,KC_TRANSPARENT,KC_TRANSPARENT,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_TRANSPARENT,KC_UP,KC_7,KC_8,KC_9,KC_ASTR,KC_F12,KC_DOWN,KC_4,KC_5,KC_6,KC_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_AMPR,KC_1,KC_2,KC_3,KC_BSLASH,KC_TRANSPARENT,KC_TRANSPARENT,KC_DOT,KC_0,KC_EQUAL,KC_TRANSPARENT,RGB_TOG,RGB_SLD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_HUD,RGB_HUI),
+  [1] = KEYMAP(KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_TRANSPARENT,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,KC_TRANSPARENT,KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LBRACKET,KC_RBRACKET,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_LABK,KC_RABK,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_SPACE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_PLUS,KC_KP_MINUS,KC_KP_ASTERISK,KC_KP_SLASH,KC_KP_ENTER,LGUI(LSFT(KC_EQUAL)),KC_UP,KC_7,KC_8,KC_9,KC_TRANSPARENT,KC_TRANSPARENT,KC_DOWN,KC_4,KC_5,KC_6,KC_TRANSPARENT,KC_TRANSPARENT,LGUI(LSFT(KC_MINUS)),KC_AMPR,KC_1,KC_2,KC_3,KC_TRANSPARENT,KC_TRANSPARENT,KC_0,KC_DOT,KC_EQUAL,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_ENTER,KC_BSPACE),
 
-  [2] = KEYMAP(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN1,KC_MS_BTN2),
+  [2] = KEYMAP(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LGUI(LSFT(KC_LBRACKET)),LGUI(LSFT(KC_RBRACKET)),KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,LALT(KC_UP),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,LALT(KC_DOWN),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGUI(KC_LBRACKET),RGUI(KC_RBRACKET)),
 
 };
 
@@ -37,14 +37,14 @@ const uint16_t PROGMEM fn_actions[] = {
 // leaving this in place for compatibilty with old keymaps cloned and re-compiled.
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-      switch(id) {
-        case 0:
-        if (record->event.pressed) {
-          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        }
-        break;
+  switch(id) {
+    case 0:
+      if (record->event.pressed) {
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
       }
-    return MACRO_NONE;
+      break;
+  }
+  return MACRO_NONE;
 };
 
 void matrix_init_user(void) {
@@ -70,9 +70,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case RGB_SLD:
       if (record->event.pressed) {
-        #ifdef RGBLIGHT_ENABLE
-          rgblight_mode(1);
-        #endif
+#ifdef RGBLIGHT_ENABLE
+        rgblight_mode(1);
+#endif
       }
       return false;
       break;
@@ -83,68 +83,68 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 uint32_t layer_state_set_user(uint32_t state) {
 
-    /* uint8_t layer = biton32(state); */
-    /*  */
-    /* ergodox_board_led_off(); */
-    /* ergodox_right_led_1_off(); */
-    /* ergodox_right_led_2_off(); */
-    /* ergodox_right_led_3_off(); */
-    /* switch (layer) { */
-    /*   case 0: */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_0 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0); */
-    /*     #endif */
-    /*     break; */
-    /*   case 1: */
-    /*     ergodox_right_led_1_on(); */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_1 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1); */
-    /*     #endif */
-    /*     break; */
-    /*   case 2: */
-    /*     ergodox_right_led_2_on(); */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_2 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2); */
-    /*     #endif */
-    /*     break; */
-    /*   case 3: */
-    /*     ergodox_right_led_3_on(); */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_3 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3); */
-    /*     #endif */
-    /*     break; */
-    /*   case 4: */
-    /*     ergodox_right_led_1_on(); */
-    /*     ergodox_right_led_2_on(); */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_4 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4); */
-    /*     #endif */
-    /*     break; */
-    /*   case 5: */
-    /*     ergodox_right_led_1_on(); */
-    /*     ergodox_right_led_3_on(); */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_5 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5); */
-    /*     #endif */
-    /*     break; */
-    /*   case 6: */
-    /*     ergodox_right_led_2_on(); */
-    /*     ergodox_right_led_3_on(); */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_6 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6); */
-    /*     #endif */
-    /*     break; */
-    /*   case 7: */
-    /*     ergodox_right_led_1_on(); */
-    /*     ergodox_right_led_2_on(); */
-    /*     ergodox_right_led_3_on(); */
-    /*     #ifdef RGBLIGHT_COLOR_LAYER_7 */
-    /*       rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6); */
-    /*     #endif */
-    /*     break; */
-    /*   default: */
-    /*     break; */
-    /* } */
+  uint8_t layer = biton32(state);
+
+  ergodox_board_led_off();
+  ergodox_right_led_1_off();
+  ergodox_right_led_2_off();
+  ergodox_right_led_3_off();
+  switch (layer) {
+    case 0:
+      #ifdef RGBLIGHT_COLOR_LAYER_0
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+      #endif
+      break;
+    case 1:
+      ergodox_right_led_1_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_1
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+      #endif
+      break;
+    case 2:
+      ergodox_right_led_2_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_2
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+      #endif
+      break;
+    case 3:
+      ergodox_right_led_3_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_3
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+      #endif
+      break;
+    case 4:
+      ergodox_right_led_1_on();
+      ergodox_right_led_2_on();
+      #ifdef RGBLIGHT_COLOR_LAYER_4
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+      #endif
+      break;
+    case 5:
+      ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_5
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+        #endif
+        break;
+      case 6:
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_6
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      case 7:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_7
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      default:
+        break;
+    }
     return state;
 
 };

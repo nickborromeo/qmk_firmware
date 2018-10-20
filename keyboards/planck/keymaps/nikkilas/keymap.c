a5132f6f4e4d10ff9a48307d3fc152974c1a2b3c
Nick Borromeo <ngborromeo@gmail.com>
2018-10-19 23:31:59 -0700

    setup raise layer for planck

--- keyboards/planck/keymaps/nikkilas/keymap.c
+++ keyboards/planck/keymaps/nikkilas/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,           KC_Q,    KC_W,     KC_E,    KC_R,      KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_T(KC_ESCAPE), KC_A,    KC_S,     KC_D,    KC_F,      KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,          KC_Z,    KC_X,     KC_C,    KC_V,      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_LSFT,          KC_Z,    KC_X,     KC_C,    KC_V,      KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
     LOWER,            RAISE,   KC_GRAVE, KC_LALT, KC_LGUI,   KC_SPC,  KC_SPC,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, LCTL(LGUI(KC_SPACE))
 ),
 
@@ -118,22 +118,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, KC_SPC,  KC_SPC,  _______, _______, _______, _______,   SGUI_T(KC_5)
 ),
 
+
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |Pg Up |Pg Dn |      |
+ * | Full | Mute | Vol- | Vol+ |      |      |      |   7  |   8  |   9  |   -  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * |  LH  |  RH  |      |      |      |      |      |   4  |   5  |   6  |   +  |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |   1  |   2  |   3  |   *  |Enter |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |    Space    |   0  |      |      |   /  |      |
  * `-----------------------------------------------------------------------------------'
  */
+
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    LALT(LGUI(KC_F)),          KC_MUTE,                    KC_VOLD, KC_VOLU, _______, _______, _______, KC_7,  KC_8,    KC_9,    KC_PMNS,  KC_BSPC,
+    LALT(LCTL(LGUI(KC_LEFT))), LALT(LCTL(LGUI(KC_RIGHT))), _______, _______, _______, _______, _______, KC_4,  KC_5,    KC_6,    KC_PPLS,  _______,
+    _______,                   _______,                    _______, _______, _______, _______, _______, KC_1,  KC_2,    KC_3,    KC_PAST,  KC_ENT,
+    _______,                   _______,                    _______, _______, _______, KC_SPC,  KC_SPC,  KC_0,  _______, _______, KC_PSLS,  _______,
 ),
 
 /* Plover layer (http://opensteno.org)

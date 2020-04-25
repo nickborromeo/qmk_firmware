ece75d1fdb6a6e70adb39f2e54f08979a8e9a434
Nick Borromeo <ngborromeo@gmail.com>
2020-04-25 16:59:24 -0700

    Update keympa file with latest changes from qmk master

--- keyboards/ergodox_infinity/keymaps/nikkilas/keymap.c
+++ keyboards/ergodox_infinity/keymaps/nikkilas/keymap.c
@@ -1,8 +1,11 @@
 #include QMK_KEYBOARD_H
-#include "ergodox_infinity.h"
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
+
+#define BASE 0 // default layer
+#define LAYER_ONE 1 // symbols
+#define LAYER_TWO 2 // media keys
 
 
 /* #include "keymap_german.h" */
@@ -60,7 +63,7 @@ KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_TRANSPARENT,KC_TRANSPARENT,KC_EXLM,KC
 // Right Side
 KC_KP_PLUS,KC_KP_MINUS,KC_KP_ASTERISK,KC_KP_SLASH,KC_KP_ENTER,LCTL(KC_RBRACKET),KC_UP,KC_7,KC_8,KC_9,KC_TRANSPARENT,LCTL(KC_RBRACKET),KC_DOWN,KC_4,KC_5,KC_6,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AMPR,KC_1,KC_2,KC_3,KC_TRANSPARENT,KC_TRANSPARENT,KC_0,KC_DOT,KC_EQUAL,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_ENTER,KC_BSPACE),
 
-  [2] = KEYMAP(
+  [LAYER_TWO] = LAYOUT_ergodox(
 
 // Left Side
 KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LGUI(LSFT(KC_LBRACKET)),LGUI(LSFT(KC_RBRACKET)),KC_TRANSPARENT,
@@ -71,28 +74,26 @@ KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_UP,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRAN
 };
 
 
-
 const uint16_t PROGMEM fn_actions[] = {
-  [1] = ACTION_LAYER_TAP_TOGGLE(1)
+    [1] = ACTION_LAYER_TAP_TOGGLE(LAYER_ONE)                // FN1 - Momentary Layer 1 (Symbols)
 };
 
-// leaving this in place for compatibilty with old keymaps cloned and re-compiled.
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
-  switch(id) {
-    case 0:
-      if (record->event.pressed) {
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        }
+        break;
+        case 1:
+        if (record->event.pressed) { // For resetting EEPROM
+          eeconfig_init();
+        }
+        break;
       }
-      break;
-  }
-  return MACRO_NONE;
-};
-
-void matrix_init_user(void) {
-#ifdef RGBLIGHT_COLOR_LAYER_0
-  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
-#endif
+    return MACRO_NONE;
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -112,81 +113,42 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
     case RGB_SLD:
       if (record->event.pressed) {
-#ifdef RGBLIGHT_ENABLE
-        rgblight_mode(1);
-#endif
+        #ifdef RGBLIGHT_ENABLE
+          rgblight_mode(1);
+        #endif
       }
       return false;
       break;
-
   }
   return true;
 }
 
-uint32_t layer_state_set_user(uint32_t state) {
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
 
-  uint8_t layer = biton32(state);
+};
 
-  ergodox_board_led_off();
-  ergodox_right_led_1_off();
-  ergodox_right_led_2_off();
-  ergodox_right_led_3_off();
-  switch (layer) {
-    case 0:
-      #ifdef RGBLIGHT_COLOR_LAYER_0
-        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
-      #endif
-      break;
-    case 1:
-      ergodox_right_led_1_on();
-      #ifdef RGBLIGHT_COLOR_LAYER_1
-        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
-      #endif
-      break;
-    case 2:
-      ergodox_right_led_2_on();
-      #ifdef RGBLIGHT_COLOR_LAYER_2
-        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
-      #endif
-      break;
-    case 3:
-      ergodox_right_led_3_on();
-      #ifdef RGBLIGHT_COLOR_LAYER_3
-        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
-      #endif
-      break;
-    case 4:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      #ifdef RGBLIGHT_COLOR_LAYER_4
-        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
-      #endif
-      break;
-    case 5:
-      ergodox_right_led_1_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_5
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
-        #endif
-        break;
-      case 6:
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_6
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
-        #endif
-        break;
-      case 7:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
-        #ifdef RGBLIGHT_COLOR_LAYER_7
-          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
-        #endif
-        break;
-      default:
-        break;
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+
+    uint8_t layer = biton32(layer_state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+      // TODO: Make this relevant to the ErgoDox EZ.
+        case 1:
+            ergodox_right_led_1_on();
+            break;
+        case 2:
+            ergodox_right_led_2_on();
+            break;
+        default:
+            // none
+            break;
     }
-    return state;
 
 };

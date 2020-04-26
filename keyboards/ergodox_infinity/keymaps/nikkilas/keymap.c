d10f19bef4b008b9d68a549e83c14ef0df94ac4c
Nick Borromeo <ngborromeo@gmail.com>
2020-04-25 17:03:25 -0700

    Format base layer

--- keyboards/ergodox_infinity/keymaps/nikkilas/keymap.c
+++ keyboards/ergodox_infinity/keymaps/nikkilas/keymap.c
@@ -26,36 +26,59 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Keymap 0: Basic layer
  *
+ *  O - Option
+ *  C - CMD
+ *  SGUI - Shift + CMD
+ *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |  !   |   @  |   #  |   $  |   %  |      |           | Alfrd|   ^  |   &  |   *  |   ~  |   +  |   -    |
+ * |   `    |  !   |   @  |   #  |   $  |   %  | OC+F |           | Alfrd|   ^  |   &  |   *  |   ~  |   +  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  |   |           |  _   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Esc    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   "    |
+ * |Esc/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   "    |
  * |--------+------+------+------+------+------|  =   |           |  :   |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * |LShft/( |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |RShift/)|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1 |  MOD  |  MOD |Option| Apple|                                       |  <   |   >  |  MOD |  MOD | ~L1  |
+ *   |  L2 |  OC+H | OC+L |Option| Apple|                                       |  [/{ |  ]/} |      |      |  L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |   {  |   [  |       |   ]  |    }   |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | MOD  |       |   '  |        |      |
- *                                 | Space|Ctrl  |------|       |------|  Ente  |Backsp|
- *                                 |      |      | L2   |       | Leadr|        |ace   |
- *                                 `--------------------'       `----------------------'
+ *                                        |SGUI+4|SGUI+5|       |GUI+{ | GUI+}|
+ *                                 ,------|------|------|       |------+--------+-----.
+ *                                 |      |      | MOD  |       |      |       |      |
+ *                                 | Space| Apple|------|       |------| Enter |Bcksp |
+ *                                 |      |      | L1   |       | Leadr|       |      |
+ *                                 `--------------------'       `---------------------'
  */
 
-  [0] = KEYMAP(
+  [BASE] = LAYOUT_ergodox(
 
 // Left Side
-KC_GRAVE,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,LALT(LGUI(KC_F)),KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_PIPE,LCTL_T(KC_ESCAPE),KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSPO,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_EQUAL,MO(2),LALT(LGUI(KC_H)),LALT(LGUI(KC_L)),KC_LALT,KC_LGUI,KC_LBRACKET,KC_RBRACKET,LCTL(LSFT(KC_S)),KC_SPACE,LCTL_T(KC_ESCAPE),MO(1),
+KC_GRAVE,          KC_EXLM,          KC_AT,             KC_HASH,  KC_DLR,  KC_PERC, LALT(LGUI(KC_F)),
+KC_TAB,            KC_Q,             KC_W,              KC_E,     KC_R,    KC_T,    KC_PIPE,
+LCTL_T(KC_ESCAPE), KC_A,             KC_S,              KC_D,     KC_F,    KC_G,
+KC_LSPO,           KC_Z,             KC_X,              KC_C,     KC_V,    KC_B,    KC_EQUAL,
+MO(2),             LALT(LGUI(KC_H)), LALT(LGUI(KC_L)),  KC_LALT,  KC_LGUI,
+
+// Left Thumb Cluster
+          SGUI(KC_4), SGUI(KC_5),
+                      LCTL(LSFT(KC_S)),
+KC_SPACE, KC_LGUI,    MO(1),
 
 // Right Side
-LCTL(KC_SPACE),KC_CIRC,KC_AMPR,KC_ASTR,KC_TILD,KC_PLUS,KC_MINUS,KC_UNDS,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,KC_COLN,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSPC,KC_LABK,KC_RABK,LALT(LCTL(LGUI(KC_LEFT))),LALT(LCTL(LGUI(KC_RIGHT))),MO(1),LGUI(KC_C),LGUI(KC_V),LGUI(KC_X),KC_COMMA,KC_ENTER,KC_BSPACE),
+LCTL(KC_SPACE),  KC_CIRC,  KC_AMPR,        KC_ASTR,        KC_TILD,    KC_PLUS,   KC_MINUS,
+KC_UNDS,         KC_Y,     KC_U,           KC_I,           KC_O,       KC_P,      KC_BSLASH,
+KC_H,            KC_J,     KC_K,           KC_L,           KC_SCOLON,  KC_QUOTE,
+KC_COLN,         KC_N,     KC_M,           KC_COMMA,       KC_DOT,     KC_SLASH,  KC_RSPC,
+KC_LBRC,         KC_RBRC,  KC_TRANSPARENT, KC_TRANSPARENT, MO(1),
+
+// Right Thumb Cluster
+          LGUI(KC_LCBR), LGUI(KC_RCBR),
+          KC_TRANSPARENT,
+KC_COMMA, KC_ENTER, KC_BSPACE),
 
 
-  [1] = KEYMAP(
+
+  [LAYER_ONE] = LAYOUT_ergodox(
 
 // Left Side
 KC_ESCAPE,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_TRANSPARENT,KC_TRANSPARENT,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE,KC_TRANSPARENT,KC_TRANSPARENT,KC_HASH,KC_DLR,KC_LBRACKET,KC_RBRACKET,KC_GRAVE,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_LABK,KC_RABK,KC_TILD,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_SPACE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,
